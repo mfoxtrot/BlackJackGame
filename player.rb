bc57b4b3bc4452497c2906c31ae7e2804f22ce1c
mfoxtrot <dmitrey.sokolov@gmail.com>
2017-11-02 23:51:31 +0300

    calc_score function

--- player.rb
+++ player.rb
@@ -1,18 +1,26 @@
 class Player
   attr_reader :name
   attr_reader :hand
-  attr_reader :score
+  attr_accessor :score
   attr_accessor :bank
 
   def initialize(name, initial_bank)
     @name = name
-    @hand = {}
+    @hand = []
     @score = 0
     @bank = initial_bank
   end
 
   def hit(card)
     hand << card
-    score += card.score
+    calc_score
+  end
+
+  def calc_score
+    scores_array = []
+    hand.each { |card| scores_array << card[:score] }
+    @score = 0
+    scores_array.each { |sc| @score += sc }
+    scores_array.each { |sc| @score -= 10 if @score > 21 && sc == 11 }
   end
 end
